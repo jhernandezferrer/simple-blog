9770b2e2b0b925e71d478396bf7405bdb039aca5
Jose Hernandez <pepeh93@hotmail.es>
2022-01-14 15:33:04 +0100

    Added rich-tech

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :authors
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   root to: "home#index"
+  scope module: 'authors' do # Authors::PostsController -> The way to solve this is creating a Author Folder
+    resources :posts do
+      resources :elements
+    end
+  end
 end
